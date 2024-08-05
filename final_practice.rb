935bac6d0b28ef958d1e9f5e8be8054d14b12a7d
Cole Berreman <c.berrem@gmail.com>
2024-08-04 19:22:39 -0600

    final_practice

--- final_practice.rb
+++ final_practice.rb
@@ -21,8 +21,15 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting
+  "Hello!"
+end 
+
+simple_greeting = greeting
+
+puts simple_greeting
+# What is the return value of your method? The return value of this method is "Hello!"
+# How many arguments did you pass your method? No arguments
 
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
@@ -31,9 +38,20 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def custom_greeting (name)
+  "Hello, #{name}"
+end 
+
+puts custom_greeting ('Cole')
+
+personalized_greeting = custom_greeting('Cole')
+puts personalized_greeting
+
+
+
+# What is the return value of your method? Hello, Cole
+# How many arguments did you pass your method? One argument
+# What data type was your argument(s)? String
 
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
@@ -41,10 +59,19 @@ puts last_laugh
 # Call the method at least twice, and store the return value in a variable:
 
 # Use the puts or print command to see the return value in the console:
+def greets_person (name1, name2, name3)
+  "Hello, #{name1} #{name2} #{name3}! How are you?"
+end 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts greets_person('Cole','Wesley','Berreman')
+
+full_name_greeting = greets_person('Cole','Wesley','Berreman')
+
+puts full_name_greeting
+
+# What is the return value of your method? Hello, Cole Wesley Berreman! How are you?
+# How many arguments did you pass your method? 3 arguments
+# What data type was your argument(s)? string
 
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
@@ -52,18 +79,41 @@ puts last_laugh
 # Call the method at least twice, and store the return value in a variable:
 
 # Use the puts or print command to see the return value in the console:
+def square(num)
+  num * num
+end
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts square(9)
+
+squared_number = square(9)
+puts squared_number
+
+
+# What is the return value of your method? 81
+# How many arguments did you pass your method? One argument
+# What data type was your argument(s)? Integer
 
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
 
 # 5: Write a method named "check_stock" that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+def check_stock(quantity, item)
+  if quantity < 3
+    puts "Stock of #{item} is running low"
+    elsif
+    puts "Stock of #{item} is adequate"
+  end
+end
 
+  puts check_stock(3, "Cheese")
+
+stock = check_stock(3,"Cheese")
+
+puts stock
+# => "Stock of Cheese is adequate"
 check_stock(4, "Coffee");
+
 # => "Coffee is stocked"
 
 check_stock(3, "Tortillas");
